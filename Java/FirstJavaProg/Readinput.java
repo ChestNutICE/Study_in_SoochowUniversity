6ddab9543b014c296c7873f09dc3757340ada6ef
栗栗安 <114720311+ChestNutICE@users.noreply.github.com>
2026-07-19 10:20:01 +0800

    Delete Java directory

--- Java/FirstJavaProg/Readinput.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class Readinput {
-    public static void main(String[] args) {
-        System.out.println(new Scanner(System.in).nextLine());
-    }
-}
--- Java/JAR/usehello.java
+++
@@ -1,5 +0,0 @@
-public class usehello {
-    public static void outout(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
